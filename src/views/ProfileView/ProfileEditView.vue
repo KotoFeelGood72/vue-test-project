e331ff07fba18e927a8a709474f812a1480ab9e3
KotoFeelGood72 <kotenkolove@yandex.ru>
2025-08-07 23:16:36 +0300

    Формирую экран редактирования пользователя

--- src/views/ProfileView/ProfileEditView.vue
+++ src/views/ProfileView/ProfileEditView.vue
@@ -1,7 +1,101 @@
 <template>
-  <div></div>
+  <div>
+    <FileInput
+      v-model="user.image"
+      name="profile-image"
+      id="profile-image"
+      :disabled="false"
+      :required="false"
+    />
+    <div class="profile_edit__inputs">
+      <CustomInput
+        v-model="user.name"
+        name="profile-name"
+        id="profile-name"
+        placeholder="Введите имя"
+        type="text"
+        className=""
+        :style="''"
+        :disabled="false"
+        :required="false"
+        :readonly="false"
+      />
+      <CustomInput
+        v-model="user.name"
+        name="profile-name"
+        id="profile-name"
+        placeholder="Введите имя"
+        type="text"
+        className=""
+        :style="''"
+        :disabled="false"
+        :required="false"
+        :readonly="false"
+      />
+      <CustomInput
+        v-model="user.name"
+        name="profile-name"
+        id="profile-name"
+        placeholder="Введите имя"
+        type="text"
+        className=""
+        :style="''"
+        :disabled="false"
+        :required="false"
+        :readonly="false"
+      />
+      <CustomInput
+        v-model="user.name"
+        name="profile-name"
+        id="profile-name"
+        placeholder="Введите имя"
+        type="text"
+        className=""
+        :style="''"
+        :disabled="false"
+        :required="false"
+        :readonly="false"
+      />
+    </div>
+    <div class="profile_edit__buttons">
+      <CustomButton
+        text="Сохранить"
+        type="submit"
+        disabled
+        loading
+        theme="primary"
+        :onClick="onProfileSave"
+      />
+      <CustomButton
+        text="Отмена"
+        type="button"
+        disabled
+        loading
+        theme="primary"
+        :onClick="cancelProfileEdit"
+      />
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CustomButton from '@/components/ui/CustomButton.vue'
+import FileInput from '@/components/input/FileInput.vue'
+import CustomInput from '@/components/input/CustomInput.vue'
+import { useUserStoreRefs } from '@/stores/useUserStore'
+import { useRouter } from 'vue-router'
+
+const { user } = useUserStoreRefs()
+const router = useRouter()
+
+const onProfileSave = () => {
+  // Логика сохранения профиля
+  console.log('Сохранение профиля')
+}
+
+const cancelProfileEdit = () => {
+  router.push('/profile')
+}
+</script>
 
 <style scoped></style>
