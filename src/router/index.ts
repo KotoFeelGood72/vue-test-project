85cb90a6b26d39e9a14175056fdc6d0648929272
KotoFeelGood72 <kotenkolove@yandex.ru>
2025-08-07 20:36:05 +0300

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/views/HomeView.vue'),
+    },
+    {
+      name: 'profile',
+      path: '',
+      children: [
+        {
+          path: 'edit',
+          component: () => import('@/views/ProfileView/PrrofileEditView.vue'),
+        },
+        {
+          path: '/profile',
+          component: () => import('@/views/ProfileView/ProfileView.vue'),
+        },
+      ],
+    },
+  ],
+})
+
+export default router
